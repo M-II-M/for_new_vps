6fb03b7318e8df78451b054e0afe028e7e726279
Mihal Ivanov <60715096+M-II-M@users.noreply.github.com>
2023-11-10 16:33:01 +0500

    Create add_user.sh

---
+++ add_user.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PASS_BACK=$1 # пароль backend
+PASS_FRONT=$2 # пароль frontend
+
+# создание юзеров
+useradd backend -m && $(echo "backend:${PASS_BACK}" |chpasswd)
+useradd frontend -m && $(echo "frontend:${PASS_FRONT}" |chpasswd)
+
+# добавление в группу sudo
+usermod -aG sudo backend
+usermod -aG sudo frontend
+
+# добавление в группу docker
+usermod -aG docker backend
+usermod -aG docker frontend
+
+# добавление в группу docker
+usermod -aG docker backend
+usermod -aG docker frontend
